d99c098ec5c922469b005673ac9c804d8f098f38
Maksym Adamneko <maimav56@gmail.com>
2024-07-03 12:37:52 +0300

    Main app rdy

--- main/urls.py
+++ main/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from main import views
+
+app_name = "main"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("main.urls", namespace="main")),
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import context
 
 
 # Create your views here.
 def index(request):
-    context = {"title": "Home", "content": "Main page"}
-    return render(request, "main/index.html", context=context)
+    context = {"title": "Home - main", "content": "Furniture shop"}
+    return render(request, "main/index.html", context)
 
 
 def about(request):
-    return HttpResponse("About page")
+    context = {
+        "title": "Home - About us",
+        "content": "We are very good",
+        "text_on_page": "Very good goods",
+    }
+    return render(request, "main/about.html", context)
