e68d3359aeee103c55828ac98e408532f06b4da6
Maksym Adamneko <maimav56@gmail.com>
2024-07-07 20:37:03 +0300

    Profe form, controllers and users RDY

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,11 @@
 from dataclasses import fields
 from pyexpat import model
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    UserCreationForm,
+    UserChangeForm,
+)
 
 from users.models import User
 
@@ -13,12 +17,18 @@ class UserLoginForm(AuthenticationForm):
         fields = ["username", "password"]
 
 
-# username = forms.CharField()
-# password = forms.CharField()
-# class UserLoginForm(forms.ModelForm):
-#     class Meta:
-#         model = User
-#         fields = ["username", "password"]
+# username = forms.CharField(
+#     label = 'Имя',
+#     widget=forms.TextInput(attrs={"autofocus": True,
+#                                   'class': 'form-control',
+#                                   'placeholder': 'Введите ваше имя пользователя'})
+# )
+# password = forms.CharField(
+#     label = 'Пароль',
+#     widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
+#                                       'class': 'form-control',
+#                                       'placeholder': 'Введите ваш пароль'})
+# )
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -39,3 +49,107 @@ class UserRegistrationForm(UserCreationForm):
     email = forms.CharField()
     password1 = forms.CharField()
     password2 = forms.CharField()
+
+    # first_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваше имя",
+    #         }
+    #     )
+    # )
+    # last_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите вашу фамилию",
+    #         }
+    #     )
+    # )
+    # username = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваше имя пользователя",
+    #         }
+    #     )
+    # )
+    # email = forms.CharField(
+    #     widget=forms.EmailInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваш email *youremail@example.com",
+    #         }
+    #     )
+    # )
+    # password1 = forms.CharField(
+    #     widget=forms.PasswordInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваш пароль",
+    #         }
+    #     )
+    # )
+    # password2 = forms.CharField(
+    #     widget=forms.PasswordInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Поддтвердите ваш пароль",
+    #         }
+    #     )
+    # )
+
+
+class ProfileForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = (
+            "image",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+        )
+
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    # image = forms.ImageField(
+    #     widget=forms.FileInput(attrs={"class": "form-control mt-3"}), required=False
+    # )
+    # first_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваше имя",
+    #         }
+    #     )
+    # )
+    # last_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите вашу фамилию",
+    #         }
+    #     )
+    # )
+    # username = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваше имя пользователя",
+    #         }
+    #     )
+    # )
+    # email = forms.CharField(
+    #     widget=forms.EmailInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваш email *youremail@example.com",
+    #             # 'readonly': True,
+    #         }
+    #     ),
+    # )
--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
-from django.contrib import auth
-from users.forms import UserLoginForm, UserRegistrationForm
-from django.contrib.auth.models import UserManager
+from django.contrib import auth, messages
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 # Create your views here.
@@ -16,6 +16,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{username} logged in")
                 return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserLoginForm()
@@ -31,6 +32,7 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(request, f"{user.username} was registered")
             return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserRegistrationForm()
@@ -39,11 +41,25 @@ def registration(request):
     return render(request, "users/registration.html", context)
 
 
+@login_required
 def profile(request):
-    context = {"title": "User profile"}
+    if request.method == "POST":
+        form = ProfileForm(
+            data=request.POST, instance=request.user, files=request.FILES
+        )
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile was updated")
+            return HttpResponseRedirect(reverse("user:profile"))
+    else:
+        form = ProfileForm(instance=request.user)
+
+    context = {"title": "User Profile", "form": form}
     return render(request, "users/profile.html", context)
 
 
+@login_required
 def logout(request):
+    messages.success(request, f"{request.user.username} was logged out")
     auth.logout(request)
     return redirect(reverse("main:index"))
