6e5999cfca30526839e0a88b182906ef8157895e
Maksym Adamneko <maimav56@gmail.com>
2024-07-08 19:28:25 +0300

    Remove and add from/to cart ajax and json response

--- carts/views.py
+++ carts/views.py
@@ -1,12 +1,16 @@
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 
+from carts.utils import get_user_carts
 from goods.models import Products
 from carts.models import Cart
 
 
 # Create your views here
-def cart_add(request, product_slug):
-    product = Products.objects.get(slug=product_slug)
+def cart_add(request):
+    product_id = request.POST.get("product_id")
+    product = Products.objects.get(id=product_id)
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
@@ -19,10 +23,38 @@ def cart_add(request, product_slug):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
-    return redirect(request.META["HTTP_REFERER"])
+    user_cart = get_user_carts(request)
+
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request
+    )
+    response_data = {
+        "message": "Product was added",
+        "cart_items_html": cart_items_html,
+    }
+    return JsonResponse(response_data)
 
 
 def cart_change(request, product_slug): ...
 
 
-def cart_remove(request, product_slug): ...
+def cart_remove(request):
+
+    cart_id = request.POST.get("cart_id")
+    cart = Cart.objects.get(id=cart_id)
+
+    qty = cart.quantity
+
+    cart.delete()
+
+    user_cart = get_user_carts(request)
+
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request
+    )
+    response_data = {
+        "message": "Product was deleted",
+        "cart_items_html": cart_items_html,
+        "quantity_deleted": qty,
+    }
+    return JsonResponse(response_data)
